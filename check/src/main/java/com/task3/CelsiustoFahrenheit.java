cf7751cca842a82fcb3395439ef2443cfc3ad543
surendhar-s <47440802+surendhar-s@users.noreply.github.com>
2020-07-08 20:12:51 +0530

    Update CelsiustoFahrenheit.java

--- check/src/main/java/com/task3/CelsiustoFahrenheit.java
+++ check/src/main/java/com/task3/CelsiustoFahrenheit.java
@@ -11,12 +11,20 @@ public class CelsiustoFahrenheit {
 	
   
   
-	@RequestMapping("/fahernheit")
-	public int fahrenheit()
+	@RequestMapping("/fahernheit/{degree}")
+	public @ResponseBody int fahrenheit(@PathVariable(value="someID") String degree)
 	{
-	    int celsius=50;
+	    int celsius=Integer.parseInt(degree);
 		int fahrenheit=((celsius*9/5)+32);
 		return fahrenheit;
 		
 	}
+	@RequestMapping("/celsius/{degree}")
+public @ResponseBody int celsius(@PathVariable(value="someID") String degree)
+{
+	int fahrenheit=Integer.parseInt(degree);
+	int celsius=(fahrenheit-32)*5/9;
+	return celsius;
+	
+}
 }
